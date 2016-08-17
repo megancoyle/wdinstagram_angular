550ea7a93e3fa2ae0affdd6c76e90a17799e51fb
Megan Coyle <meg@mcoyle.com>
2016-08-16 20:30:05 -0400

    Add file framework and logic of app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular.module("wdinstagram", [
+    "ui.router",
+    "photos"
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider,
+  $locationProvider){
+    $locationProvider.html5mode(true);
+    $stateProvider
+    .state("wdinstagramIndex", {
+      url: "/photos",
+      templateUrl: "js/photos/index.html",
+      controller: "WdinstagramIndexController",
+      controllerAs: "WdinstagramIndexViewModel"
+    })
+    .state("wdinstagramShow", {
+      url: "/photos/:id",
+      templateUrl: "js/photos/show.html",
+      controller: "WdinstagramShowContoller",
+      controllerAs: "WdinstagramShowViewModel"
+    });
+  }
 })();
